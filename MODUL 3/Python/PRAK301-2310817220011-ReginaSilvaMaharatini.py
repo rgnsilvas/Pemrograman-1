ce0ca755c1ceeacc9e2b1a6fb32d654f5627a74d
Regina Silva Maharatini <144782353+rgnsilvas@users.noreply.github.com>
2023-10-31 08:51:27 +0800

    Delete MODUL 3 directory

--- MODUL 3/Python/PRAK301-2310817220011-ReginaSilvaMaharatini.py
+++
@@ -1,6 +0,0 @@
-n1, n2 = map(int, input("Input\n").split())
-
-if n1 < n2:
-    print(f"{n1} {n2}")
-else:
-    print(f"{n2} {n1}")
--- MODUL 3/Python/PRAK303-2310817220011-ReginaSilvaMaharatini.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Input\n"))
-
-print("\nOutput")
-if num == 0:
-    print("nol")
-elif num > 0:
-    print("positif")
-else:
-    print("negatif")
